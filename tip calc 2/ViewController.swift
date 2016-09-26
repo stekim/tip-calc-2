920aedddbebb03c3bcb34240dfd1de3b48317524
Steve Kim <ski@fb.com>
2016-09-25 21:30:40 -0700

    Initial Commit

---
+++ tip calc 2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  tip calc 2
+//
+//  Created by Steve Kim on 9/25/16.
+//  Copyright © 2016 Steve Kim. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
